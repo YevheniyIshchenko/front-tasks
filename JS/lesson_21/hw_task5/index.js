449576dcb0c66f2bed654f4662e1efb76185c16f
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-01 20:15:27 +0300

    task

---
+++ JS/lesson_21/hw_task5/index.js
@@ -0,0 +1,8 @@
+export const setButton = (buttonText) => {
+  const title = document.querySelector('body');
+  title.innerHTML = `<button>${buttonText}</button>`;
+};
+
+// setButton('Click me');
+
+
