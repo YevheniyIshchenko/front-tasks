86452d401f4c62e2a34c35cf160ef2f22b7e1181
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-01 18:29:16 +0300

    task

---
+++ JS/lesson_21/hw_task2/index.js
@@ -0,0 +1,17 @@
+export const getItemList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+};
+
+export const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll('.tool');
+  const arrList = Array.from(elementsArray);
+  console.dir(arrList);
+  return arrList;
+};
+
+getItemList();
+console.log(getItemList());
+getItemsArray();
+console.log(getItemsArray());
