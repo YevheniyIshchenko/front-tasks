e2a0de373714e2e982f65764cb12998f837ad230
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-02 10:06:46 +0300

    task

---
+++ JS/lesson_21/hw_task9/index.js
@@ -0,0 +1,13 @@
+export const finishForm = () => {
+  const inputLogin = document.createElement('input');
+  inputLogin.setAttribute('type', 'text');
+  inputLogin.setAttribute('name', 'login');
+
+  const formElement = document.querySelector('.login-form');
+  formElement.prepend(inputLogin);
+
+  const foormInput = document.querySelector('input[name="password"]');
+  foormInput.setAttribute('type', 'password');
+};
+
+// finishForm();
