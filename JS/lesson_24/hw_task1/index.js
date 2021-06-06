c16b7b71e70f5e6a1a07d0a1fc7fb7d9bca92bf0
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-06 23:31:32 +0300

    hw

--- JS/lesson_24/hw_task1/index.js
+++ JS/lesson_24/hw_task1/index.js
@@ -3,12 +3,13 @@ export const getDiff = (startDate, endDate) => {
   const yers = diff.getUTCFullYear() - 1970;
   const months = diff.getUTCMonth();
 
-  let days;
-  if (yers > 0) {
-    days = diff.getUTCDate() + yers * 365;
-  } else {
-    days = diff.getUTCMonth() + months * 30;
-  }
+  // let days;
+  // if (yers > 0) {
+  //   days = diff.getUTCDate() + yers * 365;
+  // } else {
+  //   days = diff.getUTCMonth() + months * 30;
+  // }
+  const days = Math.floor(diff / (24 * 3600 * 1000));
   const hours = diff.getUTCHours();
   const minutes = diff.getUTCMinutes();
   const seconds = diff.getUTCSeconds();
@@ -21,8 +22,8 @@ export const getDiff = (startDate, endDate) => {
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
 
-// const start = new Date(2020, 5, 10, 10, 20, 10);
-// const end = new Date(2020, 6, 15, 15, 30, 16);
+const start = new Date(2020, 5, 10, 10, 20, 10);
+const end = new Date(2020, 6, 15, 15, 30, 16);
 
-// const result = getDiff(start, end);
-// console.log(result);
+const result = getDiff(start, end);
+console.log(result);
