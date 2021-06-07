ea619d4d00c259acf6f337f1a78ebf00f2195e7f
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-07 22:09:13 +0300

    t3

--- JS/lesson_27/task3/src/scripts/renderer.js
+++ JS/lesson_27/task3/src/scripts/renderer.js
@@ -3,10 +3,10 @@ import { getItem } from './storage.js';
 const listElem = document.querySelector('.list');
 
 const compareTasks = (a, b) => {
-  if (a.done !== b.done) return a.done - b.done;
-  if(a.done) return new Date(b.finishDate) - new Date(a.finishDate);
+  if (a.done - b.done !== 0) return a.done - b.done;
+  if (a.done) return new Date(b.finishDate) - new Date(a.finishDate);
   return new Date(b.createDate) - new Date(a.createDate);
-}
+};
 
 const createCheckbox = ({ done, id }) => {
   const checkboxElem = document.createElement('input');
@@ -30,7 +30,6 @@ const createListItem = ({ text, done, id }) => {
 
 export const renderTasks = () => {
   const tasksList = getItem('tasksList') || [];
-
   listElem.innerHTML = '';
   const tasksElems = tasksList.sort(compareTasks).map(createListItem);
   listElem.append(...tasksElems);
