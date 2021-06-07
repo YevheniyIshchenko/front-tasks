7d9a4e9642628904e7983ff63a1bc2a11482122f
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-07 21:26:31 +0300

    t3

--- JS/lesson_27/task3/src/scripts/createTask.js
+++ JS/lesson_27/task3/src/scripts/createTask.js
@@ -15,10 +15,10 @@ export const onCreateTask = () => {
   const newTasksList = tasksList.concat({
     text,
     done: false,
-    createDate: new Date().toISOString(),
+    finishDate: new Date().toISOString(),
     id: Math.random().toString(),
   });
-    
+
   setItem('tasksList', newTasksList);
 
   renderTasks();
