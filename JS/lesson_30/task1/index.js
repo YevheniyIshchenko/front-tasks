90c364f928e2389a0eb58a476d2387f2ae1dd379
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-09 16:00:27 +0300

    t1

--- JS/lesson_30/task1/index.js
+++ JS/lesson_30/task1/index.js
@@ -1,4 +1,4 @@
-const addImage = (imgSrc) => {
+export const addImage = (imgSrc) => {
   const p = new Promise((resolve, reject) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
