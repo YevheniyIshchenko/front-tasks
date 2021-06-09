9aa79c2b094f3058134e0f6f70adfb0beebf16e4
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-09 17:15:37 +0300

    t1

---
+++ JS/lesson_31/task1/index.js
@@ -0,0 +1,21 @@
+export const requestUserData = (userId) => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          emmail: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
+
+console.log(requestUserData('userId123'));
