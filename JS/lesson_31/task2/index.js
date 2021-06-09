edcda94de7e01499fc6c6445a729dd0765ce8b9e
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-06-09 17:42:55 +0300

    t2

--- JS/lesson_31/task2/index.js
+++ JS/lesson_31/task2/index.js
@@ -1,26 +1,24 @@
 export const asyncCalculator = (number) => {
-  const pr = new Promise((resolve) => {
+  new Promise((resolve) => {
     setTimeout(() => {
       console.log(`Initial valua: ${number}`);
       resolve(number);
     }, 500);
   })
-    .then((value) => {
-      const promise = new Promise((resolve) => {
-        setTimeout(() => {
-          const result = value * value;
-          console.log(`Squared value: ${result}`);
-          resolve(result);
-        }, 500);
-      });
-      return promise;
-    })
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+          }, 500);
+        })
+    )
     .then((value) => {
       const result = value * 2;
       console.log(`Doubled value: ${result}`);
     });
-
-  return pr;
 };
 
 asyncCalculator(2);
